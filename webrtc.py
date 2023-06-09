80af8782e73375bd2d54ac60fa6c165fb4d9f271
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-06-09 11:23:39 +0530

    alfa

--- webrtc.py
+++ webrtc.py
@@ -1,54 +1,18 @@
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
+
+
+from streamlit_webrtc import webrtc_streamer
+
+
+from streamlit_webrtc import webrtc_streamer
 import av
-import threading
+webrtc_streamer(key="sample")
 
-RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-)
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
 
-st.set_page_config(page_title="Streamlit WebRTC Demo", page_icon="🤖")
-task_list = ["Video Stream"]
+    flipped = img[::-1,:,:]
 
-with st.sidebar:
-    st.title('Task Selection')
-    task_name = st.selectbox("Select your tasks:", task_list)
-st.title(task_name)
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
-if task_name == task_list[0]:
-    style_list = ['color', 'black and white']
 
-    st.sidebar.header('Style Selection')
-    style_selection = st.sidebar.selectbox("Choose your style:", style_list)
-
-    class VideoProcessor(VideoProcessorBase):
-        def __init__(self):
-            self.model_lock = threading.Lock()
-            self.style = style_list[0]
-
-        def update_style(self, new_style):
-            if self.style != new_style:
-                with self.model_lock:
-                    self.style = new_style
-
-        def recv(self, frame):
-            # img = frame.to_ndarray(format="bgr24")
-            img = frame.to_image()
-            if self.style == style_list[1]:
-                img = img.convert("L")
-
-            # return av.VideoFrame.from_ndarray(img, format="bgr24")
-            return av.VideoFrame.from_image(img)
-
-    ctx = webrtc_streamer(
-        key="example",
-        video_processor_factory=VideoProcessor, #type: ignore
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={
-            "video": True,
-            "audio": False
-        }
-    )
-
-    if ctx.video_processor:
-        ctx.video_transformer.update_style(style_selection) #type: ignore
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
